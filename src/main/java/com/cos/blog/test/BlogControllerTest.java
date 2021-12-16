84b8da765ac2814a4054b3e8a9ad0f9f3ccea45d
sangseopkim <cy****3@gmail.com>
2021-12-16 17:30:40 +0900

    테스트 수정1

--- src/main/java/com/cos/blog/test/BlogControllerTest.java
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -8,7 +8,7 @@ public class BlogControllerTest {
 	
 	@GetMapping("/test/hello")
 	public String hello() {
-		return "<h1>hello Spring boot 3</h1><br />DB TEST..  수정2-2";
+		return "<h1>hello Spring boot 3</h1><br />DB TEST..  수정2-3";
 	}
 	
 	// root / apmsetup , db : blog
