bfa1995ba5a4853232fc3e64e60cdf52a5170d21
sangseopkim <cy****3@gmail.com>
2021-12-16 15:49:50 +0900

    STS 연동

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,15 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+	
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello Spring boot 3</h1><br />DB TEST..  수정2";
+	}
+	
+	// root / apmsetup , db : blog
+}
