787cecc6c92cd76474552defb2dfdcfa2fcf4b53
sangseopkim <cyberg3@gmail.com>
2021-12-29 15:58:28 +0900

    2021-12-29

--- src/main/java/com/cos/blog/test/BlogControllerTest.java
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -3,11 +3,17 @@ package com.cos.blog.test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class BlogControllerTest {
 	
+	private static final Logger log = LoggerFactory.getLogger(BlogControllerTest.class);
+	
 	@GetMapping("/test/hello")
 	public String hello() {
+		log.debug("log : BlogControllerTest");
 		return "<h1>hello Spring boot 3</h1><br />DB TEST..  수정3-3";
 		// 아 어렵다..
 		// 그래도 해야지.. ㅋㅋㅋ
@@ -15,13 +21,7 @@ public class BlogControllerTest {
 	
 	// root / apmsetup , db : blog
 	
-	public void test() {
-		// 13:34 분 수정
-	}
+
 	
-	public void test2() {
-		// 14:00 분 수정
-	}
-	
-	
+	// 수정
 }
